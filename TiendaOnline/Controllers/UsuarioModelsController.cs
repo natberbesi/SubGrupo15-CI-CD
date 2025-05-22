fb3d927d150a1f8ab4378eab2096ac122ae378e9
JeremahiaValeska <alejandro032893reyes@gmail.com>
2025-05-21 20:47:53 -0500

    se agregan controladores

---
+++ TiendaOnline/Controllers/UsuarioModelsController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TiendaOnline.Data;
+using TiendaOnline.Models.Domian;
+
+namespace TiendaOnline.Controllers
+{
+    public class UsuarioModelsController : Controller
+    {
+        private readonly TiendaOnlineContext _context;
+
+        public UsuarioModelsController(TiendaOnlineContext context)
+        {
+            _context = context;
+        }
+
+        // GET: UsuarioModels
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Usuarios.ToListAsync());
+        }
+
+        // GET: UsuarioModels/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var usuarioModel = await _context.Usuarios
+                .FirstOrDefaultAsync(m => m.UsuarioId == id);
+            if (usuarioModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(usuarioModel);
+        }
+
+        // GET: UsuarioModels/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UsuarioModels/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("UsuarioId,Nombre,Correo,Clave,Rol")] UsuarioModel usuarioModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(usuarioModel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(usuarioModel);
+        }
+
+        // GET: UsuarioModels/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var usuarioModel = await _context.Usuarios.FindAsync(id);
+            if (usuarioModel == null)
+            {
+                return NotFound();
+            }
+            return View(usuarioModel);
+        }
+
+        // POST: UsuarioModels/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("UsuarioId,Nombre,Correo,Clave,Rol")] UsuarioModel usuarioModel)
+        {
+            if (id != usuarioModel.UsuarioId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(usuarioModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UsuarioModelExists(usuarioModel.UsuarioId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(usuarioModel);
+        }
+
+        // GET: UsuarioModels/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var usuarioModel = await _context.Usuarios
+                .FirstOrDefaultAsync(m => m.UsuarioId == id);
+            if (usuarioModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(usuarioModel);
+        }
+
+        // POST: UsuarioModels/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var usuarioModel = await _context.Usuarios.FindAsync(id);
+            if (usuarioModel != null)
+            {
+                _context.Usuarios.Remove(usuarioModel);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UsuarioModelExists(int id)
+        {
+            return _context.Usuarios.Any(e => e.UsuarioId == id);
+        }
+    }
+}

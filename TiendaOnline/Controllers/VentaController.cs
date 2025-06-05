73e833ecc85edb334077d7a9df62d209e62a6598
natberbesi <133529196+natberbesi@users.noreply.github.com>
2025-06-05 17:46:56 -0500

    Update VentaController.cs

--- TiendaOnline/Controllers/VentaController.cs
+++ TiendaOnline/Controllers/VentaController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -162,3 +162,4 @@ namespace TiendaOnline.Controllers
         }
     }
 }
+
